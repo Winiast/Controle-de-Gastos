1a5063626c9147015600820a834540a0c29cf8b6
Gustavo Ferreira <82901156+Winiast@users.noreply.github.com>
2022-08-14 13:36:43 -0300

    Projeto em reactjs de controle financeiro

---
+++ src/components/header/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 150px;
+  text-align: center;
+  background-color: #8bc6ec;
+  background-color: #4d7979;
+  background-image: linear-gradient(0deg, #4d7979 0%, #4ebf8a 100%);
+`;
+
+export const Header = styled.h1``;
+
+export const Title = styled.div`
+  padding-top: 20px;
+  color: #fff;
+`;
